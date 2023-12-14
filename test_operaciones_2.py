e32946da6abbd6c4df3b7d309b435bb879993f29
Dcanales <152242012+dcanalesv633@users.noreply.github.com>
2023-12-13 22:10:13 -0600

    Added test cases

---
+++ test_operaciones_2.py
@@ -0,0 +1,23 @@
+import pytest
+from operaciones import suma
+
+def test_suma():
+    assert suma(2, 2) == 4
+
+
+@pytest.mark.parametrize(
+        "input_a, input_b, expected",
+        [
+            (3,2,5),
+            (2,3,5),
+            (suma(3,2),5,10),
+            (3, suma(2,5),10),
+            (7,3,10),
+            (3,7,10),
+            (7,3,10)
+        ]
+)
+def test_suma_multi(input_a, input_b, expected):
+    assert suma(input_a, input_b) == expected
+
+#https://www.youtube.com/watch?v=5ufpsjfk99U
